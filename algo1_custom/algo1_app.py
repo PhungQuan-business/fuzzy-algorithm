9331ab9cfa3661a947b169bf0c07035da9a9d358
Quan-business <phunghongquan.business@gmail.com>
2024-05-18 08:09:55 +0700

    first commit

---
+++ algo1_custom/algo1_app.py
@@ -0,0 +1,209 @@
+import numpy as np
+import os
+from algo1_custom import IntuitiveFuzzy
+from sklearn.preprocessing import LabelEncoder
+from sklearn import preprocessing
+from tabulate import tabulate
+import warnings, os, time
+from sklearn.model_selection import KFold
+import statistics 
+from operator import itemgetter
+# from utils_fuzzy import Logging
+from operator import itemgetter
+import pandas as pd
+warnings.filterwarnings("ignore")
+PATH = "C:/AR/data/"
+LOG_PATH = "logs"
+
+
+arr_data = [
+# ["movement_libras",[90], 0.001] #1 
+# ["wall",[24], 0.01] #4#
+# ["ionosphere",[34], 0.01]  #2
+# ["mfeat",[76], 0.01] # 0.1 #10
+# ["Urban",[147], 0.0005] # 9
+#["waveform2",[40], 0.01] # 3
+# ["hill-valley",[100], 0.001] # 2
+# ["Pizza",[37], 0.01] #0.8
+# ["leaf", [15], 0.001]
+
+# ["spectf",[44], 0.01] #2
+# ["cmc",[4,5,8,9], 0.01]
+# ["hill-valley",[100], 0.01] #0.05
+# ["mfeat",[76], 0.01] # 0.1
+# ["movement_libras",[90], 0.01] #0.6
+#["waveform2",[40], 0.01] #0.4
+# ["MDL",[500], 0.01] #0.7
+# ["leukemia_4",[7129], 0.05] #0.2
+# ["ORL",[1024], 0.1] #0.3  #alpha remove = 0.01
+# ["Pizza",[37], 0.01] #0.8
+# ["piechart2", [36], 0.01]
+# ["vehicle",[18], 0.01]
+# ["winequality-red",[11], 0.01]
+# ["volkert", [100], 0.001]
+# ['vowel', [0, 1, 12], 0.001]
+# ["onehun", [64], 0.01]
+# ["gesture", [32], 0.01]
+# ["fri_c2_1000",[50] , 0.01]
+# ["climate", [0,20], 0.01]
+# ["heart",[1,2,5,6,8,10,11,12,13], 0.00]
+# ["synthetic", [61], 0.01]
+# ["CLL_SUB_111",[11340], 0.05] #1
+# ["pc4",[37], 0.001] #0.02
+# ["robot-failures",[90], 0.00] 
+# ["forest", [27],0.001]
+# ["gesture",[32], 0.00]
+# ["glass",[9], 0.0]
+# ["robot-failures",[90], 0.01] #1
+# ["waveform",[21], 0.01] #0.55
+# ["thyroid",[21], 0.01] 
+# ["PlanTexture", [64], 0.01]
+# ["kc1",[21], 0.01]    
+# ["wdbc",[30], 0.01] 
+# ["warpAR10P",[2400],0.05]
+# ["madelon",[500],0.01]
+# ["sonar",[60], 0.01]
+# ["ILPD",[1,10],0.01]
+# ["heart",[1,2,5,6,8,10,11,12,13],0.01]
+# ["wine",[13], 0.01]
+# ["pollution",[15],0.025]
+# ["pyrim",[27],0.01]
+# ["ionosphere",[34], 0.025]
+# ["pc4",[37]] 0.2
+# ["robot-failures",[90]] 0.1
+# ["Urban",[147], 0.01]
+# ["hill-valley",[100]] 0.2
+# ["tecator",[124],[0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75]]
+# ["lsvt",[310],[0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75]]
+# ["pd",[754], 0.025] #0.8
+# ["ORL",[1024], 0.1] #0.8
+# ["MDL",[500], 0.05] #0.7
+# ["leukemia",[7129],[0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1]]
+# ["waveform",[21], 0.01] #0.55
+# ["warpAR10P",[2400],[0.55,0.55,0.55,0.55,0.55,0.55,0.55,0.55,0.55,0.55]]
+# ["micro",[1300]]
+# ["qsar",[41], 0.01]
+# ["spambase",[57],[0.1,0.55,0.75,0.55,0.55,0.55,0.75,0.55,0.55,0.25]]
+# ["lung",[3312]]
+# ["lung_discrete",[325]]
+# ["SRBCT",[2308],[0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1]]
+# ["robot-failures",[90]]*
+# ["Image",[135,136,137,138,139],[0.1,0.25,0.25,0.25,0.1,0.1,0.1,0.55,0.1,0.25]]
+# ["texture",[40]]
+# ["scene",[294,295,296,297,298,299],[0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001]]
+# ["autouniv",[11,30,39,40], 0.0]
+# ["german",[0,2,3,5,6,8,9,11,13,14,16,18,19,20], 0.001]
+# ["vehicle",[18], 0.01]
+# ["seismic-bumps",[0,1,2,7,18],0.01]  
+# ["waveform2",[40], 0.01] #0.4
+# ["cmc",[4,5,8,9], 0.01]
+# ["wall",[24]] 0.2
+# ["satimage",[36], 0.01]
+# ["ozone", [72], 0.05]
+# ["qsar",[41], [0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75]],# 0.8
+# ["segmentation",[19],0.01], #0.85
+# ["mfeat",[76], 0.01] 
+# ["mfeat",[76]],
+# ["sick", [29]]
+# ["triazines", [60],0.01]
+# ["agnostic",[48], 0.01] 
+# ["parkinsons", [22], 0.01]
+# ["oil", [49], 0.01]  
+# ["pc3",[37], 0.01]
+# ["synthetic",[61], 0.01]
+# ["spectrometer",[101], 0.025]
+# ["Pizza",[37], 0.0]
+# ["piechart3",[37], 0.01]
+#  ["person",[321], 0.01]
+# ["Yale",[1024]]
+# ["spectf",[44], 0.01]
+    ]
+
+min_max_scaler = preprocessing.MinMaxScaler()
+
+def preprocessing(file_path, att_nominal_cate=None):
+    '''
+    Input:
+        file_path: path to dataset
+        att_nominal_cate: indexes of categorical attributes(optional)
+
+    Return:
+        A dataset with minmaxscalerd attributes and encoded categorical attributes
+    '''
+    
+    # Read the dataset
+    data = pd.read_csv(file_path, header=None, delimiter=',')
+    DS = data.to_numpy()
+    
+    # Get all columns indices
+    all_columns = np.arange(DS.shape[1])
+    
+    if att_nominal_cate is not None:
+        att_nominal_cate = np.array(att_nominal_cate)
+        # Encode nominal categories
+        for i in att_nominal_cate:
+            DS[1:, i] = LabelEncoder().fit_transform(DS[1:, i])
+        
+        # Get columns that are not nominal categories
+        att_real = np.setdiff1d(all_columns, att_nominal_cate)
+    else:
+        att_real = all_columns
+
+    # Apply MinMaxScaler to non-nominal columns
+    if len(att_real) > 0:
+        DS[1:, att_real] = min_max_scaler.fit_transform(DS[1:, att_real])
+
+    return DS
+
+def split_data(data, number=1):
+    if number == 1:
+        return [data]
+    ldt = len(data)
+    spt = int(ldt / number)
+    blk = spt * number
+    arrs = np.split(data[:blk], number)
+    if blk != ldt:
+        arrs[-1] = np.vstack((arrs[-1], data[blk:]))
+    return arrs
+
+def split_data_icr(data):
+    arrs = []
+    arrs_2 = split_data(data, number=2)
+    arrs.append(arrs_2[0])
+    arrs_2[1] = split_data(arrs_2[1], number=5) # change as preferred
+    for arr in arrs_2[1]:
+        arrs.append(arr)
+    return arrs
+
+def main(arr_data):
+    start = time.time()
+    a_sc = [["Data","|C|", "|R_F|", "Acc_O","std_O", "Acc_F", "std_F", "T_F", "Reduct"]]
+    n_steps = 2
+    B = []
+
+    for arr in arr_data:
+        F = []
+        print('pass1')
+        DS = preprocessing(arr[0], arr[1])
+        print('pass2')
+        st = time.time()
+        DS = split_data_icr(DS)
+        print('pass3')
+        IF = IntuitiveFuzzy(DS[0], arr[0])
+        print('pass4')
+        F, time_filter = IF.filter()
+        print("F", F)
+        print('pass5')
+        sc = IF.evaluate(arr[0], F, time_filter)
+        print('pass6')
+        a_sc.append(sc)
+        print(tabulate(a_sc, headers='firstrow', tablefmt='pipe', stralign='center'))    
+
+    print(time.time() - start)
+
+if __name__ == "__main__":
+    file_path = '/Users/phunghongquan/Documents/NCS-VietAnh/algorithm_custom/data/libras_movement/movement_libras.data'
+    arr_data = [[file_path, None]]  # Ensure this is a list of lists with file paths and optional indices
+    main(arr_data)
+
+
---
+++ algo1_custom/algo1_custom.py
@@ -0,0 +1,324 @@
+'''
+float32 for all
+sau khi tìm được set dis thì vẫn cần phải ép kiểu numpy array
+
+'''
+
+
+""" Read Page 3 """
+import numpy as np
+import time, os, pickle
+import pandas as pd
+from functools import reduce
+from tabulate import tabulate
+from sklearn import svm, tree
+from sklearn.model_selection import cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+
+import time
+import numpy as np
+
+class IntuitiveFuzzy:
+    def __init__(self, data, att_nominal_cate=None):
+        """Khởi tạo tham số đầu vào
+        Input:
+            2d numpy array: bảng dữ liệu đầu vào ở dạng numpy, chưa transpose
+
+        Yields:
+            condition attribute matrix: a 2d numpy array of data sample without decision attribute
+            decision attribute vector: a 1d numpy vector of decision attribute
+        """
+        
+        '''
+        Input:
+            data: data chưa transpose
+            att_data: bảng attributes không có cate attr
+            C: decision variable
+        '''
+        # Input numpy matrix
+        self.data = data
+        # for calculating
+        self.att_nominal_cate = att_nominal_cate
+        # self.attributes = range(0,len(self.data[0]))
+        # self.arr_real = [i for i in self.attributes  if i not in att_nominal_cate]
+        self.cond_attr = data.T[:-1].astype(np.float32)
+        self.dec_attr = data.T[-1].astype(np.float32)
+        self.attributes = range(0,len(self.data[0]))
+        
+        self.chose_idx = []  # List to keep track of chosen indices
+        
+    # đầu vào matrix không bao gồm condition attribute
+
+    def cal_fuzzy_correlation(self):
+        """Tính các matrix quan hệ mờ cho từng C_i
+        Input:
+            matrix đã được transpose và không bao gồm condition attribute
+
+        Return:
+            fuzzy_cor_list: list các matrix quan hệ tương quan mờ
+        """
+        fuzzy_corr = []
+        for sample in self.cond_attr:
+            arr_2d = np.broadcast_to(sample.reshape(-1, 1), (len(sample), len(sample))).astype(np.float32)
+            arr_cols = np.broadcast_to(sample, (len(sample), len(sample))).astype(np.float32)
+            equal = arr_2d == arr_cols
+            output = np.where(equal, 1.0, np.minimum(arr_2d, arr_cols)).astype(np.float32)
+            fuzzy_corr.append(output)        
+        return fuzzy_corr
+
+    def cal_M_C(self):
+        """Tính matrix M_C từ các matrix tương quan mờ
+        Input:
+            fuzzy_corr_matrix: list of fuzzy correlation matrix
+
+        Return:
+            M_C: a 2d numpy matrix
+        """
+        fuzzy_corr_matrix = self.cal_fuzzy_correlation()
+        M_C = np.min(fuzzy_corr_matrix, axis=0).astype(np.float32)
+        return M_C
+    
+    def cal_lambda(self):
+        """Tính lambda cho từng C_i
+
+        Input:
+            M_C: 2d numpy array
+        
+        Return:
+            lambda_matrix: a 2d numpy matrix
+        """
+        M_C = self.cal_M_C()
+        inverse_matrix = (1.0 - M_C).astype(np.float32)
+        np.fill_diagonal(inverse_matrix, np.inf)
+        min_values_matrix = np.min(inverse_matrix, axis=1).astype(np.float32)
+        lambda_2d = np.broadcast_to(min_values_matrix.reshape(-1,1), (len(min_values_matrix), len(min_values_matrix))).astype(np.float32)
+        return lambda_2d
+
+    def partition_mask(self):
+        """Tính matrix mask để bỏ đi những giá trị cùng phân cấp khi tính discernibility set
+
+        Input:
+            decision attribute vector: a 1d numpy array of condition attribute matrix 
+
+        Return:
+            mask: a 2d numpy array of masked valued
+        """
+        cond_att = self.dec_attr
+        arr_2d = np.broadcast_to(cond_att.reshape(-1, 1), (len(cond_att), len(cond_att))).astype(np.float32)
+        mask = (arr_2d.T == arr_2d).astype(np.float32)
+        return mask
+
+    def cal_dis_set(self):
+        """Dùng cho tính toán các cặp discernibility của từng C_i.
+        A list of sub-list. Each sub-list is a collection of 
+        unique discernibility set for each C_i
+        
+        Input:
+            condition attributes matrix: a 2d nunpy array of data sample
+
+        Return:
+            A list of unique discernibility set for each C_i
+            a list of length of each C_i discenibility collection
+        """
+        mask = self.partition_mask()
+        lambda_matrix = self.cal_lambda()
+
+        all_dis_set = []
+        all_len_dis_set = []
+        for sample in self.cond_attr:
+            arr_2d = np.broadcast_to(sample.reshape(-1, 1), (len(sample), len(sample))).astype(np.float32)
+            arr_cols = np.broadcast_to(sample, (len(sample), len(sample))).astype(np.float32)
+            output = (1.0 - np.minimum(arr_2d, arr_cols)).astype(np.float32)
+            output[mask==1] = -1.0
+            dis = np.where(output >= lambda_matrix)
+            dis = list(zip(dis[0], dis[1]))
+            dis_len = len(dis)
+            all_len_dis_set.append(dis_len)
+            all_dis_set.append(dis)
+        return all_dis_set, all_len_dis_set
+    
+    def cal_permutation_dis_set(self): 
+        '''Tính chỉnh hợp của các discernibility set
+
+        Input:
+            list of list: list of all discernibility
+        
+        Return:
+            int: length of the permutation set
+        '''
+        concatenated_list = []
+        all_dis_set,  all_len_dis_set = self.cal_dis_set()    
+        for inner_list in all_dis_set:
+            concatenated_list.extend(inner_list)
+            concatenated_list = list(set(concatenated_list))
+        len_concatenated_list = len(concatenated_list)
+        return all_dis_set, len_concatenated_list, all_len_dis_set
+
+    def filter(self):
+        start = time.time()
+        A = []  # List to hold unique elements after each merge
+        all_dis_set, len_per_list, all_len_dis = self.cal_permutation_dis_set()
+
+        max_idx = all_len_dis.index(max(all_len_dis))
+        self.chose_idx.append(max_idx)
+        A.extend(all_dis_set[max_idx])
+
+        if len(A) >= len_per_list:
+            finish_time = time.time() - start
+            return self.chose_idx, finish_time
+        else:
+            temp_B = {}  # Initialize the dictionary outside the loop
+            while len(A) < len_per_list:
+                for idx, dis_set in enumerate(all_dis_set):
+                    if idx not in self.chose_idx:
+                        copy_A = A.copy()
+                        copy_A.extend(dis_set)
+                        copy_A = list(set(copy_A))
+                        if len(copy_A) >= len_per_list:
+                            self.chose_idx.append(idx)
+                            finish_time = time.time() - start
+                            return self.chose_idx, finish_time
+                        else:
+                            temp_B[idx] = len(copy_A)
+
+                if temp_B:
+                    idx_max_value = max(temp_B, key=temp_B.get)
+                    A.extend(all_dis_set[idx_max_value])
+                    A = list(set(A))
+                    self.chose_idx.append(idx_max_value)
+                    temp_B.clear()  # Clear temp_B to recalculate in the next iteration
+                else:
+                    print("Error: temp_B is empty. No valid combination found.")
+                    break
+        finish_time = time.time() - start
+        return self.chose_idx, finish_time
+    
+    def scores(self, reduct):
+
+		# y_test = self.data[:,-1]
+		# y_test = y_test.astype(int)
+		# print(reduct)
+		# list_index = [self.attributes[:-1].index(i) for i in reduct]
+		# X_test = self.data[:,list_index]
+		# print(list_index)
+
+        y_train = self.data[:,-1]
+        y_train = y_train.astype(int)
+        X_train = self.data[:, reduct]
+
+
+        #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=0)
+        # clf = tree.DecisionTreeClassifier()
+        # clf = svm.SVC(kernel='rbf', C=1, random_state=42).fit(X_train, y_train)
+        # clf = KNeighborsClassifier(n_neighbors=10).fit(X_train, y_train)
+        # clf = svm.SVC(kernel='rbf', C=1, random_state=42)
+        clf = KNeighborsClassifier(n_neighbors=5)
+        H = cross_val_score(clf, X_train, y_train, cv=10)
+        acc = round(H.mean(), 3)
+        std = round(np.std(H), 3)
+        # scores = round(cross_val_score(clf, X_train, y_train, cv=10).mean(),3)
+        # scores = clf.score(X_test, y_test)
+        #self.arr_acc.append(scores)
+
+        return acc, std     
+	
+    def update_dataset(self, data):
+        '''
+            This is a function to update incremental dataset
+            Params:
+                - data: new dataset
+        '''
+        self.data = data
+        # self.prev = num_prev
+
+    def update_n_objs(self):
+        '''
+            This is a function to update a new number of objects
+        '''
+        # self.num_prev += self.num_objs
+        # self.num_delta = self.num_objs - self.num_prev
+        self.num_objs = len(self.data[:,0])
+        # self.dis_tg = self.dis_tg
+	
+    def update_dis(self, dis):
+        self.dis_tg = dis
+
+    def update_n_attribute(self, B):
+        '''
+            This is a function to update a new list of attributes
+            Params:lear
+                - B: list of attribute after wrapper phase of previous step.
+        '''
+        # TODO: save reduct set into self.B
+        # self.dis_tg = dis_tg
+        self.B = B
+
+    def update_retional_matrices(self):
+        '''
+            This is a function to update relational_matrices
+        '''
+        self.relational_matrices = self._get_single_attr_IFRM(self.data)
+        
+
+    def evaluate(self, name, reduct_f, time_f):
+        file_name = os.path.basename(name)
+		# print("reduct_f", reduct_f)
+		# cf = tree.DecisionTreeClassifier()
+		# cf= svm.SVC(kernel='rbf', C=1, random_state=42)
+        cf = KNeighborsClassifier(n_neighbors=5)
+
+		# y_test= self.data[:,-1]
+		# y_test = y_test.astype(int)
+        y_train = self.data[:,-1]
+        y_train = y_train.astype(int)
+		
+		
+		# X_test_o = self.data[:,:-1]
+        X_train_o = self.data[:,:-1]
+
+
+        clf_o = cf.fit(X_train_o, y_train)
+		# scores_o = round(clf_o.score(X_test_o, y_test),3)
+		# clf = KNeighborsClassifier(n_neighbors=10)
+        H_o = cross_val_score(clf_o, X_train_o, y_train, cv=10)
+        scores_o = round(H_o.mean(),3)
+        std_o = round(np.std(H_o), 3)
+
+
+
+		# Calculate Filter
+		# reduct_f = reduct_f[-1]
+		# X_test = self.data[:, reduct_f]
+        X_train = self.data[:, reduct_f]
+
+        clf = cf.fit(X_train, y_train)
+        # scores_f = round(clf.score(X_test, y_train),3)
+        H_f = cross_val_score(clf, X_train, y_train, cv=10)
+        scores_f = round(H_f.mean(),3)
+        std_f = round(np.std(H_f), 3)
+
+        return (file_name, len(self.attributes)-1, len(reduct_f),
+                scores_o, std_o, scores_f, std_f, round(time_f,3), self.chose_idx)
+
+
+# Example usage
+# con_attr = [
+#     [0.2, 0.6, 1.0, 0.1, 0.0],
+#     [0.9, 0.5, 0.3, 0.8, 2.0],
+#     [0.6, 0.8, 0.6, 0.4, 1.0],
+#     [0.3, 0.6, 0.9, 1.0, 0.0]
+# ]
+
+def main():
+    file_path = '/Users/phunghongquan/Documents/NCS-VietAnh/algorithm_custom/data/libras_movement/movement_libras.data'
+    data = pd.read_csv(file_path, header=None, delimiter=',')
+    data_array = data.to_numpy()
+
+    algo = IntuitiveFuzzy(data_array)
+    
+    result, elapsed_time = algo.filter()
+    print(result, elapsed_time)
+
+if __name__ == "__main__":
+    main()
+    
